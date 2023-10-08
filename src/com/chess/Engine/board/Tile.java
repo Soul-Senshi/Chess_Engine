c1ef6aeb86ac4597a98c3b0b979eb0bfa96522ac
Soul Senshi <soulsenshi100@gmail.com>
2023-10-08 12:53:42 +0300

    Added Immutability and Guava

--- src/com/chess/Engine/board/Tile.java
+++ src/com/chess/Engine/board/Tile.java
@@ -1,10 +1,31 @@
 package com.chess.Engine.board;
 
-import com.chess.Engine.pieces.*;
-public abstract class Tile {
-    int tileCoordinate;
+import com.chess.Engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
 
-    Tile(int tileCoordinate) {
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Tile {
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for(int i = 0; i < 64; i++){
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
+
+    private Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
     }
 
@@ -12,7 +33,7 @@ public abstract class Tile {
     public abstract Piece getPiece();
 
     public static final class EmptyTile extends Tile {
-        EmptyTile(int coordinate) {
+        EmptyTile(final int coordinate) {
             super(coordinate);
         }
 
@@ -29,7 +50,7 @@ public abstract class Tile {
 
     public static final class OccupiedTile extends Tile {
 
-        Piece pieceOnTile;
+        private final Piece pieceOnTile;
 
         OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
             super(tileCoordinate);
